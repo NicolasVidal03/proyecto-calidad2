fcbe01f42798760606015296891aa53449c484f5
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-10-13 16:40:05 -0400

    se elimino prueba duplicada

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -6,25 +6,6 @@ describe('CatalogoKata Class', () => {
         expect(true).toBe(true);
     });
     
-    it('deberia mostrar vacio en caso de que no hay elementos en el catalogo', () => {
-        const catalogo = new CatalogoKata(); 
-        const resultado = catalogo.mostrarCatalogoKatas();
-        expect(resultado).toBe("");
-    });
-
-    it('deberia mostrar mensajes concatenados cuando existen elementos en el catalogo', () => {
-        const catalogo = new CatalogoKata();
-        const kata1 = new Kata("Kata 1", "Belen", "Descripcion Belen", "Facil");
-        const kata2 = new Kata("Kata 2", "Nico", "Descripcion Nico", "Medio");
-        const kata3 = new Kata("Kata 3", "Sebas", "Descripcion Sebas", "Dificil");
-        catalogo.listaKatas.push(kata1);
-        catalogo.listaKatas.push(kata2);
-        catalogo.listaKatas.push(kata3);
-        
-        const resultado = catalogo.mostrarCatalogoKatas();
-        expect(resultado).toBe(kata1.mostrar() + kata2.mostrar() + kata3.mostrar());
-    });
-    
 })
 describe('mostrarCatalogoKatas method', () => {
     
