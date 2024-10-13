357d5b4b640b515e162bb185c9a2bea8af134d51
Belen <beckiar23@gmail.com>
2024-10-13 10:37:54 -0400

    Se añadieron pruebas unitarias para el metodo buscarPorEstado

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -1,7 +1,32 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { CatalogoKata, Kata } from '../src/katas'; 
 
 describe('CatalogoKata Class', () => {
-    it('should always be true', () => {
-        expect(true).toBe(true);
+    let catalogo;
+
+    beforeEach(() => {
+        catalogo = new CatalogoKata();
+    });
+
+    describe('buscarPorEstado method', () => {
+        it('should return an empty list when the list of katas is empty', () => {
+            const resultado = catalogo.buscarPorEstado("Terminado");
+            expect(resultado.length).toBe(0);
+        });
+        it('should return an empty list when there are no katas with the searched state', () => {
+            const kata1 = new Kata("Kata 1", "Autor A", "Descripción 1", "Difícil");
+            kata1.setEstado("Terminado");
+            catalogo.agregarKata(kata1);
+            const resultado = catalogo.buscarPorEstado("No terminado");
+            expect(resultado.length).toBe(0);
+        }); 
+        it('should return a list with matches when there are katas with the searched state', () => {
+            const kata1 = new Kata("Kata 1", "Autor A", "Descripción 1", "Fácil");
+            kata1.setEstado("Terminado");
+            catalogo.agregarKata(kata1);
+            const resultado = catalogo.buscarPorEstado("Terminado");
+            expect(resultado.length).toBe(1);
+            expect(resultado).toContain(kata1);
+        });
     });
 })
