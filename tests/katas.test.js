b366293140bcad9e33fb7c3da27470be71fcf3c0
Belen <beckiar23@gmail.com>
2024-10-12 22:52:29 -0400

    Se añadieron 3 casos de prueba para asegurar la cobertura del método

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -1,7 +1,53 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { CatalogoKata, Kata } from '../src/katas'; 
 
 describe('CatalogoKata Class', () => {
-    it('should always be true', () => {
-        expect(true).toBe(true);
+
+    let catalogo;
+
+    beforeEach(() => {
+        catalogo = new CatalogoKata();
+    });
+
+    describe('ordenarPorDescripcion method', () => {
+        it('should maintain the same order when descriptions are equal', () => {
+            const kata1 = new Kata("Kata 1", "Autor 1", "Descripción Igual", "Fácil");
+            const kata2 = new Kata("Kata 2", "Autor 2", "Descripción Igual", "Medio");
+        
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+        
+            catalogo.ordenarPorDescripcion();
+        
+            const listaOrdenada = catalogo.getLista();
+            expect(listaOrdenada[0].getDescripcion()).toBe("Descripción Igual");
+            expect(listaOrdenada[1].getDescripcion()).toBe("Descripción Igual");
+        });
+        it('should sort correctly when descripcionA is lexicographically greater than descripcionB', () => {
+            const kata1 = new Kata("Kata 1", "Autor 1", "Zebra", "Fácil");
+            const kata2 = new Kata("Kata 2", "Autor 2", "Águila", "Medio");
+        
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+        
+            catalogo.ordenarPorDescripcion();
+        
+            const listaOrdenada = catalogo.getLista();
+            expect(listaOrdenada[0].getDescripcion()).toBe("Zebra");
+            expect(listaOrdenada[1].getDescripcion()).toBe("Águila");
+        });
+        it('should sort correctly when descripcionA is lexicographically greater than descripcionB', () => {
+            const kata1 = new Kata("Kata 1", "Autor 1", "Águila", "Fácil");
+            const kata2 = new Kata("Kata 2", "Autor 2", "Zebra", "Medio");
+        
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+        
+            catalogo.ordenarPorDescripcion();
+        
+            const listaOrdenada = catalogo.getLista();
+            expect(listaOrdenada[0].getDescripcion()).toBe("Zebra");
+            expect(listaOrdenada[1].getDescripcion()).toBe("Águila");
+        });
     });
 })
