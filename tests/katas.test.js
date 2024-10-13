a01571e29ccc8a06d9bc47b187ab4ffb8c248eb0
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-10-13 17:36:33 -0400

    se aniadio pruebas para el metodo buscarPorDescripcion, deberia devolver un array vacio [] cuando la descripcion no coincida con ninguna kata

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -167,6 +167,19 @@ describe('buscarPorDescripcion method', () => {
         expect(resultado.length).toBe(1);
         expect(resultado).toContain(kata1);
     });
+    it('deberia devolver un array vacio [] cuando la descripcion no coincida con ninguna kata', () => {
+        const catalogo = new CatalogoKata(); 
+        const kata1 = new Kata("Kata 1", "Belen", "Descripcion Sebas", "Facil");
+        const kata2 = new Kata("Kata 2", "Nico", "Descripcion Nico", "Medio");
+        const kata3 = new Kata("Kata 3", "Sebas", "Descripcion Sebas", "Dificil");
+        catalogo.listaKatas.push(kata1);
+        catalogo.listaKatas.push(kata2);
+        catalogo.listaKatas.push(kata3);
+        
+        const resultado = catalogo.buscarPorDescripcion("Descripcion");
+        expect(resultado.length).toBe(0);
+        expect(resultado).toEqual([]);
+    });
     
 })
 
