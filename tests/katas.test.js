741daac6c816f93fa03ccd844d453c45eea503ac
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-10-13 15:54:48 -0400

    se aniadio pruebas para el metodo mostarCatalogoKatas, deberia mostrar vacio en caso de que no hay elementos en el catalogo

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -1,7 +1,16 @@
 import { describe, it, expect } from 'vitest';
+import { CatalogoKata, Kata } from '../src/katas';
 
 describe('CatalogoKata Class', () => {
     it('should always be true', () => {
         expect(true).toBe(true);
     });
+
+    it('deberia mostrar vacio en caso de que no hay elementos en el catalogo', () => {
+        const catalogo = new CatalogoKata(); 
+        const resultado = catalogo.mostrarCatalogoKatas();
+        expect(resultado).toBe("");
+    });
+
 })
+
