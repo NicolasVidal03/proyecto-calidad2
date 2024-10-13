af85304139663c4d04db4e2e179d22e78878903d
Belen <beckiar23@gmail.com>
2024-10-12 20:14:37 -0400

    Basic configuration

---
+++ tests/katas.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('CatalogoKata Class', () => {
+    it('should always be true', () => {
+        expect(true).toBe(true);
+    });
+})
---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8', // Usa v8 para la cobertura
+      reporter: ['text', 'html', 'lcov'], // Asegúrate de incluir 'lcov' aquí
+      reportsDirectory: './coverage', // Elige el directorio para los reportes de cobertura
+    },
+  },
+});
