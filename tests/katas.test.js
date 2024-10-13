8c1d855002895fd091f1b1d5fee9d30682df0176
Belen <beckiar23@gmail.com>
2024-10-13 09:50:13 -0400

    Se añadieron las pruebas para el metodo bsucarPorAutor

--- tests/katas.test.js
+++ tests/katas.test.js
@@ -1,7 +1,31 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect,beforeEach } from 'vitest';
+import { CatalogoKata, Kata } from '../src/katas'; 
 
 describe('CatalogoKata Class', () => {
-    it('should always be true', () => {
-        expect(true).toBe(true);
+    let catalogo;
+
+    beforeEach(() => {
+        catalogo = new CatalogoKata();
     });
+
+    describe('buscarPorAuthor method', () => {
+        it('should return an empty list when the kata list is empty', () => {
+            const resultados = catalogo.buscarPorAutor("Autor A");
+            expect(resultados.length).toBe(0); 
+        });  
+        it('should return an empty list when there are no katas with the specified author', () => {
+            const kata1 = new Kata("Kata 1", "Autor B", "Descripción 1", "Fácil");
+            catalogo.agregarKata(kata1);
+            const resultados = catalogo.buscarPorAutor("Autor A");
+            expect(resultados.length).toBe(0);
+        });
+        it('should return a list with one match when there is a kata with the specified author', () => {
+            const kata1 = new Kata("Kata 1", "Autor A", "Descripción 1", "Fácil");        
+            catalogo.agregarKata(kata1);        
+            const resultados = catalogo.buscarPorAutor("Autor A");
+            expect(resultados.length).toBe(1);
+            expect(resultados[0].getAutor()).toBe("Autor A");
+        });
+        
+    })
 })
