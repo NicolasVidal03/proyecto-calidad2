a7aababa65ffbb90cbd6b7eaba2cd76cfcedb316
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-10-18 10:13:06 -0400

    se aniadio prueba para el metodo getNombre de la clase Usuario, deberia devolver la descripcion del usuario despues de instanciar la clase Usuario

--- tests/usuario.test.js
+++ tests/usuario.test.js
@@ -12,6 +12,17 @@ describe('metodo getNombre', () => {
         expect(resultado).not.toBeUndefined();
         expect(resultado).toBe('Juan');
     });
+
 });
 
+describe('metodo getDesc', () => {
+    it('deberia devolver la descripcion del usuario despues de instanciar la clase Usuario', () => {
+        const usuario = new Usuario('Juan', 'Descripcion');
+        const resultado = usuario.getDesc();
 
+        expect(resultado.length).toBeGreaterThanOrEqual(2);  
+        expect(resultado.length).toBeLessThanOrEqual(100);    
+        expect(resultado).not.toBeUndefined();
+        expect(resultado).toBe('Descripcion');
+    });
+});
